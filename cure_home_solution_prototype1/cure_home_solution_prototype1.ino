bffe91fef16c6ec2024b1c9e7a0197bbd420d04a
ChristianJeremi98 <christianjeremi05@gmail.com>
2018-11-13 23:10:15 +0800

    Adding firebase and wifi fix

--- cure_home_solution_prototype1/cure_home_solution_prototype1.ino
+++ cure_home_solution_prototype1/cure_home_solution_prototype1.ino
@@ -1,22 +1,22 @@
-   #include <MQ2.h>
-#include <NTPClient.h> //timestamp library
-#include <ESP8266WiFi.h> //library wifi
+#include <MQ2.h>
+#include <NTPClient.h>                                                          //timestamp library
+#include <ESP8266WiFi.h>                                                        //library wifi
 #include <WiFiUdp.h>
-#include <FirebaseArduino.h> //library firebase-arduino
+#include <FirebaseArduino.h>                                                    //library firebase-arduino
 
 
-#define WIFI_ID "WIFI_ATMA_82" //ganti isi "" dengan nama wifi
-#define WIFI_PASS "atma*@82" // ganti isi "" dengan password wifi
+#define WIFI_ID "net_print"                                                  //ganti isi "" dengan nama wifi
+#define WIFI_PASS "03634509"                                                    //ganti isi "" dengan password wifi
 
-#define FIREBASE_HOST "curehomesolution.firebaseio.com" //Firebase database
-#define FIREBASE_AUTH "xyAkQ8iYUeuf0P4FMH1JIuO06goT4ZRlAWstFzRo" //Firebase secret key
+#define FIREBASE_HOST "curehome-ee497.firebaseio.com"                         //Firebase database
+#define FIREBASE_AUTH "MrYkqOc4XLHuH6lOGyexQDnAmoZyAvsoO0cmHIIq"                //Firebase secret key
 
-#define BUZZER D3 //BUZZER
-#define MQ A0 // SENSOR ASAP
-#define RELAYCH1 D0 //RELAY LAMPU CHANNEL 1
-#define RELAYCH2 D1 // RELAY LAMPU CHANNEL 2
+#define BUZZER D3                                                               //BUZZER
+#define MQ A0                                                                   //SENSOR ASAP
+#define RELAYCH1 D0                                                             //RELAY PINTU SELENOID CHANNEL 1
+#define RELAYCH2 D1                                                             //RELAY LAMPU
 #define NTP_OFFSET 60 * 60 // detik
-#define NTP_ADDRESS "2.th.pool.ntp.org" //waktu indonesia
+#define NTP_ADDRESS "2.th.pool.ntp.org"                                         //waktu indonesia
 #define NTP_INTERVAL 120 * 1000 
 
 //TODO : WIFI UDP 
@@ -25,8 +25,12 @@ NTPClient timeClient(ntpUDP,NTP_ADDRESS,NTP_OFFSET,NTP_INTERVAL);
 String statusLampu;
 String statusPintu;
 
-StaticJsonBuffer<200> jsonBuffer; //jsoN buffer
+StaticJsonBuffer<200> jsonBuffer;                                               //JSON buffer
 MQ2 mq2(MQ); 
+
+unsigned long interval = 60000 * 1;
+unsigned long previousMillis = 0;
+int asapBahaya =1;
 
 void setup() {
   //inisiasi pin
@@ -56,6 +60,8 @@ void setup() {
 
 void loop() {
 
+  
+
   timeClient.update();
   
   cekLampu();
@@ -71,9 +77,9 @@ void cekLampu(){
   Serial.print("Lampu : ");
   Serial.println(statusLampu);
   if (statusLampu == "On"){
-    digitalWrite(RELAYCH1,HIGH);
+    digitalWrite(RELAYCH2,HIGH);
   } else if(statusLampu == "Off"){
-    digitalWrite(RELAYCH1,LOW);
+    digitalWrite(RELAYCH2,LOW);
   }
 }
 
@@ -84,9 +90,9 @@ void cekPintu(){
   Serial.print("Pintu : ");
   Serial.println(statusPintu);
   if(statusPintu=="On"){
-    digitalWrite(RELAYCH2, HIGH);
+    digitalWrite(RELAYCH1, HIGH);
   }else if(statusPintu=="Off"){
-     digitalWrite(RELAYCH2,LOW);
+     digitalWrite(RELAYCH1,LOW);
   }
 }
 
@@ -98,30 +104,42 @@ void cekAsap(){
 
    Serial.print("Kadar asap (ppm) : ");
    Serial.println(asap);
-   
-   if(asap>200){
+
+// TODO kalau kebakaran otomatis mengirim tanpa jeda sekuensial
+   if(asap>asapBahaya){
       //TODO ALARM SOUND
-      digitalWrite(BUZZER, HIGH);
+      tone(BUZZER, 2500);
+      logAsap(asap);
+   } else{
+    unsigned long currentMillis = millis();
+//    if((unsigned long)(currentMillis - previousMillis) >= interval){
+//      logAsap(asap);
+//    }
+      logAsap(asap);
+      digitalWrite(BUZZER,LOW);
+   }
    }
 
-  logAsap(asap);
-}
+  
+
 
 void logAsap(int asap){
  
-//  log asap
+//  log X asap
+
+// TODO coba pakai millis
   
   String logTime = timeClient.getFormattedTime();
   
-  JsonObject& logUser = jsonBuffer.createObject();
-  logUser["Kadar Asap"] = asap;
-  logUser["Waktu"]=logTime;
+//  JsonObject& logUser = jsonBuffer.createObject();
+//  logUser["Kadar Asap"] = asap;
+//  logUser["Waktu"]=logTime;
   
-  Firebase.push("/Log/Asap",logUser);
-  delay(1000);
+//  Firebase.push("/Log/Asap",logUser);
+//  delay(1000);
 
-  Firebase.setInt("/Asap/status",asap);
-  Firebase.setString("/Asap/time_stamp",logTime);
+  Firebase.setInt("Asap/status",asap);
+  Firebase.setString("Asap/time_stamp",logTime);
 
 }
 
